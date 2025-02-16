f49be17b97da7eca77224d2ba0afc139bfc9c9ec
AsmaaElManshawy <sayedasmaa2001@gmail.com>
2025-02-16 20:48:01 +0200

    contactEventType

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,15 +9,18 @@ import { HeaderComponent } from '../header/header.component';
 })
 export class ContactComponent {
   addcl: boolean[] = [false, false, false, false];
-  changeclass(e: KeyboardEvent): void {
+  changeclass(e: Event): void {
     const target = e.target as HTMLInputElement;
     const id: number = Number(target.id);
     const value: string = target.value;
 
     if (!value) {
       this.addcl[id] = false;
+      console.log('false', value);
+      console.log(e);
     } else {
       this.addcl[id] = true;
+      console.log('false', value);
     }
   }
 }
